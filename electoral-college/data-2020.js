0b34bb7df5ad89859e5b8640c45b855325496e9f
alienpuppy-labs <alienpuppies@gmail.com>
2026-04-05 19:01:37 -0700

    Create data-2020.js

---
+++ electoral-college/data-2020.js
@@ -0,0 +1,11 @@
+const data_2020 = {
+    meta: {
+        year: "2020",
+        blue: { name: "Biden", color: "#3b82f6" }, // Maybe a slightly different blue for 2020
+        yellow: { name: "Other", color: "#f59e0b" },
+        red: { name: "Trump", color: "#ef4444" }  // Maybe a slightly brighter red
+    },
+    states: {
+        // "Alabama": [9, ...],
+    }
+};
